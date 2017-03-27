d33ba79ac8e678a277cd8497a4445036f755a8da
Hsu <edittye379@gmail.com>
2017-03-27 16:21:16 +0800

    material design

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
 <div id="app">
-  <h1>中大即時氣象</h1>
-  <button :disabled="loading" @click="refresh()">重新整理</button>
-  <p>體感溫度：{{ apparentTemperature }}&deg;C</p>
-  <p>現在溫度：{{ wt.temperature }}&deg;C</p>
-  <p>目前降雨：{{ wt.precip }} mm/hr</p>
-  <h2>天氣預報</h2>
-  <!-- <img src="http://pblap.atm.ncu.edu.tw/getchart.asp?img=NCU&id=1027482187"> -->
-  <iframe src="http://pblap.atm.ncu.edu.tw/ncuobs/plotsta72T_track.asp" width="595"></iframe>
-  <p>資料來源：國立中央大學─大氣科學系─大氣邊界層暨空氣污染實驗室</p>
+  <md-toolbar>
+    <h1 class="md-title" style="flex: 1">中大即時氣象</h1>
+    <md-button class="md-icon-button" 
+        :disabled="loading" @click.native="refresh()"
+      >
+      <md-icon>refresh</md-icon>
+    </md-button>
+  </md-toolbar>
+
+  <div id="progress-bar">
+    <md-progress class="md-accent" md-indeterminate
+      v-show="loading"
+    ></md-progress>
+  </div>
+
+  <md-list>
+    <md-list-item>
+      <md-icon>cloud_queue</md-icon>
+      <span>體感溫度：{{ apparentTemperature }}&deg;C</span>
+      <md-spinner :md-size="30" md-indeterminate class="md-accent" v-show="loading"></md-spinner>
+    </md-list-item>
+    <md-list-item>
+      <md-icon>cloud</md-icon>
+      <span>現在溫度：{{ wt.temperature }}&deg;C</span>
+      <md-spinner :md-size="30" md-indeterminate class="md-accent" v-show="loading"></md-spinner>
+    </md-list-item>
+    <md-list-item>
+      <md-icon>grain</md-icon>
+      <span>目前降雨：{{ wt.precip }} mm/hr</span>
+      <md-spinner :md-size="30" md-indeterminate class="md-accent" v-show="loading"></md-spinner>
+    </md-list-item>
+  </md-list>
+
+  <md-card>
+    <md-card-header>
+      <div class="md-title">
+        天氣預報
+        <a href="http://pblap.atm.ncu.edu.tw" target="_blank"><md-icon class="md-accent">link</md-icon></a>
+      </div>
+      <div class="md-subhead">資料來源：http://pblap.atm.ncu.edu.tw</div>
+    </md-card-header>
+    <md-card-media>
+        <iframe src="http://pblap.atm.ncu.edu.tw/ncuobs/plotsta72T_track.asp" width="600"></iframe>
+    </md-card-media>
+    <md-card-content>
+      <h3 class="md-subheading">Powered by 國立中央大學─大氣科學系─大氣邊界層暨空氣污染實驗室</h3>
+    </md-card-content>
+  </md-card>
 </div>
 </template>
 
@@ -26,7 +65,6 @@ export default {
     }
   },
   created () {
-    // this.getNcu()
     this.getNcuAtm()
   },
   computed: {
@@ -44,8 +82,8 @@ export default {
     getNcu() {
       this.$http.get('https://ncu.herokuapp.com/ncu')
         .then( response => {
-          this.wt = response.data
           this.loading = false
+          this.wt = response.data
         })
         .catch( error => {
           console.log(error)
@@ -54,8 +92,8 @@ export default {
     getNcuAtm() {
       this.$http.get('https://ncu.herokuapp.com/ncuatm')
         .then( response => {
-          this.wt = response.data
           this.loading = false
+          this.wt = response.data
         })
         .catch( error => {
           console.log(error)
@@ -72,4 +110,7 @@ export default {
 </script>
 
 <style>
+#progress-bar {
+  height: 5px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import VueMaterial from 'vue-material'
 
 Vue.prototype.$http = axios
+Vue.use(VueMaterial)
 
 new Vue({
   el: '#app',
