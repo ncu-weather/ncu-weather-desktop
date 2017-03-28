20a28010b5fb478f90d7ba17d454fac54a80f981
Hsu <edittye379@gmail.com>
2017-03-28 08:16:51 +0800

    :bug: entry point

--- electron.js
+++ electron.js
@@ -1,38 +1,33 @@
 var url = require('url')
-const path = require('path');
-// ./main.js
+const path = require('path')
 const {app, BrowserWindow} = require('electron')
 
-let win = null;
+let win = null
 
 app.on('ready', function () {
+  win = new BrowserWindow({width: 1000, height: 600})
 
-  // Initialize the window to our specified dimensions
-  win = new BrowserWindow({width: 1000, height: 600});
-
-  // Specify entry point to default entry point of vue.js
-  win.loadURL('http://localhost:8080');
+  win.loadURL('http://localhost:8080')
   // win.loadURL(url.format({
-  //   pathname: path.join(__dirname, 'dist/index.html'),
+  //   pathname: path.join(__dirname, 'index.html'),
   //   protocol: 'file:',
   //   slashes: true
-  // }));
+  // }))
 
-  // Remove window once app is closed
   win.on('closed', function () {
-    win = null;
-  });
+    win = null
+  })
 
-});
-//create the application window if the window variable is null
+})
+
 app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
 })
-//quit the app once closed
+
 app.on('window-all-closed', function () {
   if (process.platform != 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
